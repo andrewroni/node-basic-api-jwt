8252e7375c73e7412adf2d0a149ac10e8f36c939
andreyback <andrey.nodejs01@gmail.com>
2019-07-07 13:19:47 +0300

    add responses

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,34 +1,41 @@
 const debug = require('debug')('contacts:controller*');
 
-const {Contacts} = require('../models/contacts');
+const {Contact} = require('../models/contact');
 
 exports.createContact = async (req, res, next) => {
 	const { name, phone , email } = req.body;
-	const contact = new Contacts({name, phone , email});
-	await contact.save();
+	const contact = new Contact({name, phone , email});
+	try {
+		await contact.save();
+	} catch (error) {next(error);}
 	debug('New contact: ', contact);
-	res.status(200).json(
-		{
-			success: true,
-			data: {
-				contact
-			}
-		}
-	);
+	res.status(200).json({
+		success: true,
+		data: contact
+	});
 };
 
 exports.getContacts = async (req, res, next) => {
-    
+	res.status(200).json({
+		success: true,
+		data: []
+	});
 };
 
 exports.getContact = async (req, res, next) => {
-    
+	res.status(200).json({
+		success: true
+	});
 };
 
 exports.updateContact = async (req, res, next) => {
-    
+	res.status(200).json({
+		success: true
+	});
 };
 
 exports.removeContact = async (req, res, next) => {
-    
+	res.status(200).json({
+		success: true
+	});
 };
