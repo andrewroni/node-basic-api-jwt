5cdd06b3ab1b424b115a0695b16004711b831b58
andreyback <andrey.nodejs01@gmail.com>
2019-07-07 13:47:02 +0300

    add validation error handler

--- middleware/error.js
+++ middleware/error.js
@@ -2,7 +2,13 @@
 
 module.exports = app => {
 	app.use((error, req, res, next) => {
-
+		if (error.name === 'ValidationError') {
+			return res.status(400).json({ 
+				success: false,
+				message: error.details[0].message 
+			});
+		}
+		
 		res.status(500).json({ 
 			success: false,
 			message: error.message 
@@ -10,4 +16,3 @@ module.exports = app => {
 		next();
 	});
 };
-
